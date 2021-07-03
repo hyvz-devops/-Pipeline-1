829b4a85574e51eacff247faa5508b611e330146
Halil Yavuz <68934275+hyvz-devops@users.noreply.github.com>
2021-07-03 16:48:24 -0400

    Update acr.tf

--- acr.tf
+++ acr.tf
@@ -1,1 +1,33 @@
+resource "azurerm_resource_group" "rg" {
+  name     = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "containerRegistry1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.example.id
+    ]
+  }
+
+  encryption {
+    enabled            = true
+    key_vault_key_id   = data.azurerm_key_vault_key.example.id
+    identity_client_id = azurerm_user_assigned_identity.example.client_id
+  }
+
+}
+
+resource "azurerm_user_assigned_identity" "example" {
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  name = "registry-uai"
+}
 
