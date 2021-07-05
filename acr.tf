d2602df6245569ce5caf07137e82631f5f5b765a
Cloud User <azureuser@terraform-pipeline.mpoeesq3ywceng0mit4nlbd23a.xx.internal.cloudapp.net>
2021-07-05 02:55:28 +0000

    updated codes

--- acr.tf
+++ acr.tf
@@ -1,46 +1,22 @@
-# resource "azurerm_container_registry" "acr" {
-#   name                = "containerRegistry1"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location            = azurerm_resource_group.rg.location
-#   sku                 = "Premium"
+resource "azurerm_container_registry" "acr" {
+  name                     = "containerRegistry1"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Premium"
+  admin_enabled            = false
+  storage_account_id       = "e9dd5004-4e25-49f2-a70c-3461944c0d4d"
+  georeplication_locations = ["East US", "West Europe"]
 
-#   identity {
-#     type = "UserAssigned"
-#     identity_ids = [
-#       azurerm_user_assigned_identity.example.id
-#     ]
-#   }
-
-#   encryption {
-#     enabled            = true
-#     key_vault_key_id   = data.azurerm_key_vault_key.example.id
-#     identity_client_id = azurerm_user_assigned_identity.example.client_id
-#   }
-
-# }
-
-# resource "azurerm_user_assigned_identity" "example" {
-#   resource_group_name = azurerm_resource_group.example.name
-#   location            = azurerm_resource_group.example.location
-
-#   name = "registry-uai"
-# }
-
-# resource "azurerm_sql_server" "primary" {
-#   name                         = "sql-primary"
-#   resource_group_name          = azurerm_resource_group.example.name
-#   location                     = azurerm_resource_group.example.location
-#   version                      = "12.0"
-#   administrator_login          = "sqladmin"
-#   administrator_login_password = "pa$$w0rd"
-# }
-
-# resource "azurerm_sql_server" "secondary" {
-#   name                         = "sql-secondary"
-#   resource_group_name          = azurerm_resource_group.example.name
-#   location                     = "northeurope"
-#   version                      = "12.0"
-#   administrator_login          = "sqladmin"
-#   administrator_login_password = "pa$$w0rd"
-# }
+  network_rule_set {
+    default_action = "Deny"
+    ip_rule {
+      action   = "Allow"
+      ip_range = var.a_cidr
+    }
+    # virtual_network {
+    #   action    = "Allow"
+    #   subnet_id = var.a_subnet
+    # }
+  }
+}
 
--- module.tf
+++
@@ -1,5 +0,0 @@
-module "acr" {
-  source  = "claranet/acr/azurerm"
-  version = "4.1.0"
-  # insert the 7 required variables here
-}
