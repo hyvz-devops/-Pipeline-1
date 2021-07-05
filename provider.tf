63dcc0263e3b430e34f86a49e932c0e71685cadb
Cloud User <azureuser@terraform-pipeline.mpoeesq3ywceng0mit4nlbd23a.xx.internal.cloudapp.net>
2021-07-05 23:35:40 +0000

    provider updated

--- provider.tf
+++ provider.tf
@@ -11,11 +11,4 @@ provider "azurerm" {
   features {}
 }
 
-# provider "azurerm" {
-#   features {}
 
-#   subscription_id = "e9dd5004-4e25-49f2-a70c-3461944c0d4d"
-#   client_id       = "e9dd5004-4e25-49f2-a70c-3461944c0d4d"
-#   client_secret   = var.client_secret
-#   tenant_id       = "8991588b-b8ba-4c3b-a645-ea31f6f17aeb"
-# }
